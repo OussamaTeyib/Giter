feb99cb99799730f6f106b851aebf04f64eb0987
Oussama Teyib <oussama.teyib@gmail.com>
2023-04-08 23:33:29 +0000

    Created main file

--- giter.c
+++ giter.c
@@ -12,11 +12,14 @@ int main(int argc, char *argv[])
     char *cmd = malloc(100);
     snprintf(cmd, 100, "git add %s", argv[1]);
     system(cmd);
+    printf("\nAdding is done!\n");
 
     snprintf(cmd, 100, "git commit -m \"%s\"", argv[2]);
     system(cmd);
+    printf("\nCommiting is done!\n");
 
     system("git push origin main");
+    printf("\nPushing is done!\n");
 
     free(cmd);
     return 0;
