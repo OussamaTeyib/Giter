d708d357269689b8be13ff093d60cba242e0c6f1
Oussama Teyib <oussama.teyib@gmail.com>
2023-04-09 23:31:14 +0000

    Updating

--- giter.c
+++ giter.c
@@ -4,39 +4,76 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3 || argc > 5)
+    if (argc != 3 && argc != 4)
     {
         fprintf(stderr, "Usage: giter <input-files> <message> <option>\n");
         fprintf(stderr, "Options:\n"
                         "    -n: to create a new repository.\n"
-                        "        You must pass the name!\n"
                         "    NOTHING: to push to an existant repository\n");
         return -1;
     }
-   
+    
     char *cmd = malloc(100);
-    /* if (!strcmp(argv[3], "-n"))
+    if (!cmd)
     {
-        if (!argv[4])
+        printf("Cannot allocate memory for cmd string!\n");
+        return -1;
+    }
+    
+    // if no flags passed, argv[3] is NULL. so, passing it to strcmp() causes a segfault.
+    // We check if it isn't not NULL, and then compare it with '-n'.
+    if (argv[3] && strcmp(argv[3], "-n"))
+    {  
+        char *name = malloc(100);
+        if (!cmd)
         {
-            fprintf(stderr, "Pass the name of the repository!\n");
+            printf("Cannot allocate memory for the name of the repo!\n");
             free(cmd);
             return -1;
-        }   
-        snprintf(cmd, 100, "gh repo create %s\n", argv[4]);
-        system(cmd);    
-        snprintf(cmd, 100, "git remote add https://github.com/OussamaTeyib/%s.git", argv[4]); 
-        system(cmd);
-    } */
+        }
 
+        printf("Enter the name of the repository: ");
+        fgets(name, 100, stdin);
+        name[strlen(name) - 1] = '\0';
+
+        int public;
+        printf("Make the repository public? (1/0): ");
+        scanf("%d", &public);
+
+        // create the repository
+        snprintf(cmd, 100, "gh repo create %s %s\n", name, public? "--public" : "--private");
+        system(cmd);
+        printf("Repository is created!\n");
+
+        // intialize this dir as .git repository
+        system("git init");
+        printf("Directory is initalized!\n");
+
+        // set remote URL
+        snprintf(cmd, 100, "git remote add origin https://github.com/OussamaTeyib/%s.git", name); 
+        system(cmd);
+        printf("Remote URL is set!\n");
+
+        free(name);
+    }
+
+    // set default name of the branch
+    system("git branch -m main");  
+    printf("Deafult name is set!\n");
+   
+    // add files to staging zone     
     snprintf(cmd, 100, "git add %s", argv[1]);
     system(cmd);
+    printf("Files are added!\n");
 
+    // commit the changes
     snprintf(cmd, 100, "git commit -m \"%s\"", argv[2]);
     system(cmd);
-    printf("\nCommiting is done!\n");
+    printf("Changes are commited");
 
+    // push the changes
     system("git push origin main");
+    printf("Changes are pushed\n");
 
     free(cmd);
     return 0;
